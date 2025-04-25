190bc162cc5750c7bb1a0073149fb956455adc01
Alex Skripov <scripoff.a@yandex.ru>
2025-04-25 18:46:29 +0500

    Убраны некоторые данные в подвале

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -18,10 +18,10 @@
             <div class="row">
                 <div class="col-sm-12 col-lg-4 mb-3">
                     <div><a class="footer_link" href="#">Пользовательское соглашение</a></div>
-                    <div>По вопросам работы портала обращайтесь:</div>
-                    <div><a style="text-decoration: none; color:#000" href="mailto:info@roi.ru">info@roi.ru</a></div>
-                    <div class="footer_link">8-800-200-61-62</div>
-                    <div>API РОИ</div>
+{{--                    <div>По вопросам работы портала обращайтесь:</div>--}}
+{{--                    <div><a style="text-decoration: none; color:#000" href="mailto:info@roi.ru">info@roi.ru</a></div>--}}
+{{--                    <div class="footer_link">8-800-200-61-62</div>--}}
+{{--                    <div>API РОИ</div>--}}
                 </div>
                 <div class="col-sm-12 col-lg-4 mb-3">
                     <div class="footer_link">При поддержке</div>
