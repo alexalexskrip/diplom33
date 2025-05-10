cfde5775e6620968cf49b232c6743402d6974b6d
Alex Skripov <scripoff.a@yandex.ru>
2025-05-10 11:42:20 +0500

    Небольшие изменения

--- resources/views/cabinet/projects/show.blade.php
+++ resources/views/cabinet/projects/show.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-2xl font-bold mb-6">{{ $project->name_project }}</h1>
