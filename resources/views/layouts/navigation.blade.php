e60116efecc562fb4324e0cd330ced3dc4b272f0
Alex Skripov <scripoff.a@yandex.ru>
2025-05-12 14:11:52 +0500

    Добавлена ссылка перехода на главную страницу

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -68,12 +68,16 @@
                 </div>
             </div>
 
+            <x-nav-link :href="route('home')" :active="request()->routeIs('home')" class="ml-auto" target="_blank">
+                На главную
+            </x-nav-link>
+
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            class="-mb-1 inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::prefix('cabinet')->name('cabinet.')->group(function () {
