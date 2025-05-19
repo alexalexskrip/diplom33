7b68ddb5e41802d79bd54904c6c29d7d3c880e79
Alex Skripov <scripoff.a@yandex.ru>
2025-05-19 23:25:43 +0500

    Небольшие изменения. Добавлен логотип к формам аутентификации

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,56 +1,58 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Сайт студенческих инициатив</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-@if(session()->has('success'))
-    <div class="alert alert-success" role="alert" style="position: absolute; top: 120px; right: 30px; z-index: 100; max-width: 420px">
-        <p>{{ session('success') }}</p>
-    </div>
-@endif
-@if(session()->has('wrong'))
-    <div class="alert alert-danger" role="alert" style="position: absolute; top: 120px; right: 30px; z-index: 100; max-width: 420px">
-        <p>{{ session('wrong') }}</p>
-    </div>
-@endif
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Сайт студенческих инициатив</title>
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        @if(session()->has('success'))
+            <div class="alert alert-success" role="alert" style="position: absolute; top: 120px; right: 30px; z-index: 100; max-width: 420px">
+                <p>{{ session('success') }}</p>
+            </div>
+        @endif
+        @if(session()->has('wrong'))
+            <div class="alert alert-danger" role="alert" style="position: absolute; top: 120px; right: 30px; z-index: 100; max-width: 420px">
+                <p>{{ session('wrong') }}</p>
+            </div>
+        @endif
 
-    <header>
-        <div class="top-bar">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-2">
-                        <div class="logo">
-                            <img src="{{ asset('images/imgpsh_fullsize_anim.png') }}" alt="">
+        <header>
+            <div class="top-bar">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-2">
+                            <div class="logo">
+                                <img src="{{ asset('images/imgpsh_fullsize_anim.png') }}" alt="">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-10">
-                        <div class="row align-items-center">
-                            <div class="col-7">
-                                <div class="menu">
-                                    <ul>
-                                        <li><a href="#">Как это работает?</a></li>
-                                        <li><a href="#">Обратная связь</a></li>
-                                        <li><a href="#">Вопрос-ответ</a></li>
-                                        {{--                                    <li><a href="{{ route('cabinet.dashboard') }}">Кабинет</a></li>--}}
-                                    </ul>
+                        <div class="col-10">
+                            <div class="row align-items-center">
+                                <div class="col-7">
+                                    <div class="menu">
+                                        <ul>
+                                            <li><a href="#">Как это работает?</a></li>
+                                            <li><a href="#">Обратная связь</a></li>
+                                            <li><a href="#">Вопрос-ответ</a></li>
+                                            {{--                                    <li><a href="{{ route('cabinet.dashboard') }}">Кабинет</a></li>--}}
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="text-center"><i class="fa-solid fa-phone"></i> 8 (800) 550 03 63</div>
-                            </div>
-                            <div class="col-2 text-end">
-                                <a class="main_btn" href="{{ route('cabinet.dashboard') }}">Вход</a>
+                                <div class="col-3">
+                                    <div class="text-center"><i class="fa-solid fa-phone"></i> 8 (800) 550 03 63</div>
+                                </div>
+                                <div class="col-2 text-end">
+                                    <a class="main_btn" href="{{ route('cabinet.dashboard') }}">
+                                        {{ auth()->check() ? 'Кабинет' : 'Вход' }}
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
